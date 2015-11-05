8c43a076160578cb6ccea43248e0ea8d2797406f
Simon Pickartz <spickartz@eonerc.rwth-aachen.de>
2015-11-05 16:41:03 +0100

    start to work on migration example

---
+++ scripts/migration_example.rb
@@ -0,0 +1,59 @@
+#!/usr/bin/env ruby
+
+require 'optparse'
+require 'thwait'
+
+# benchmark configuration
+PROC_QUEUE=["app0", "app1", "app2"]
+VMS={
+  PROC_QUEUE[0] => ["centos7110", "centos7111"],
+  PROC_QUEUE[1] => ["centos7112", "centos7113"]
+}
+MIGRATIONS={
+  "centos7111" => {source: "pandora3", dest: "pandora4"},
+  "centos7113" => {source: "pandora4", dest: "pandora3"},
+}
+
+
+# define command line options
+options = {}
+
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{__FILE__}"
+
+  opts.on("-v", "--verbose", "Print debug output") do |v|
+    options[:verbose] = v 
+  end
+end.parse!
+
+# start all VMs
+VMS.values.flatten.each do |vm|
+  puts "`#{Dir.pwd}/start_vm_with_xml.sh #{vm}.xml`"
+end
+
+
+# start first processes of the queue
+running_jobs = []
+running_jobs << Thread.new { puts `#{Dir.pwd}/#{PROC_QUEUE[0]}` }
+running_jobs << Thread.new { puts `#{Dir.pwd}/#{PROC_QUEUE[1]}` }
+
+# wait for first Job to terminate
+ready_job = ThreadsWait.new(running_jobs).next_wait
+puts running_jobs
+running_jobs.delete(ready_job)
+puts running_jobs
+ready_job.join
+
+# migrate in accordance with the configuration
+migrate_jobs = []
+MIGRATIONS.each do |vm, config|
+  migrate_jobs << Thread.new { puts "`#{Dir.pwd}/migrate_vm.sh #{vm} #{config[:source]} #{config[:dest]}`" }
+end
+ThreadsWait.all_waits(migrate_jobs)
+
+# start third job
+running_jobs << Thread.new { puts `#{Dir.pwd}/#{PROC_QUEUE[2]}` }
+
+running_jobs.each do |job|
+  job.join
+end
